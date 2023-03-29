1531e5323b384be51f194c900dd18af1acb41ced
Kelsey Gilbert <kelsey.gilbert@mozilla.com>
2023-03-29 16:13:39 -0700

    Add ShCompileOptions.initGLPointSize.
    Change-Id: I1d998867f3e829ed0dc8181fa76be5fe701dde70

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -1385,6 +1385,20 @@ bool TCompiler::checkAndSimplifyAST(TIntermBlock *root,
         mGLPositionInitialized = true;
     }
 
+    if (mShaderType == GL_VERTEX_SHADER && compileOptions.initGLPointSize)
+    {
+        InitVariableList list;
+        AddBuiltInToInitList(&mSymbolTable, mShaderVersion, root, "gl_PointSize", &list);
+
+        if (!list.empty()) {
+            if (!InitializeVariables(this, root, list, &mSymbolTable, mShaderVersion,
+                                     mExtensionBehavior, false, false))
+            {
+                return false;
+            }
+        }
+    }
+
     // DeferGlobalInitializers needs to be run before other AST transformations that generate new
     // statements from expressions. But it's fine to run DeferGlobalInitializers after the above
     // SplitSequenceOperator and RemoveArrayLengthMethod since they only have an effect on the AST
