4f57b17857e71492c5c5491868dd733befdf588a
Jamie Nicol <jnicol@mozilla.com>
2026-05-29 11:25:21 +0100

    Revert "Modernize for macOS 12"
    This reverts commit d28c6adf936bb811b5eb992919a0df88624eed3f.

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -757,7 +757,15 @@ void DisplayMtl::ensureCapsInitialized() const
     // Metal-Feature-Set-Tables.pdf says that max supported point size is 511. We limit it to 64
     // for now. http://anglebug.com/42263403
 
-    mNativeCaps.maxAliasedPointSize = 511;
+    // NOTE(kpiddington): This seems to be fixed in macOS Monterey
+    if (@available(macOS 12.0, *))
+    {
+        mNativeCaps.maxAliasedPointSize = 511;
+    }
+    else
+    {
+        mNativeCaps.maxAliasedPointSize = 64;
+    }
     mNativeCaps.minAliasedLineWidth = 1.0f;
     mNativeCaps.maxAliasedLineWidth = 1.0f;
 
--- src/libANGLE/renderer/metal/mtl_utils.mm
+++ src/libANGLE/renderer/metal/mtl_utils.mm
@@ -860,7 +860,12 @@ static MTLLanguageVersion GetUserSetOrHighestMSLVersion(const MTLLanguageVersion
                     case 3:
                         return MTLLanguageVersion2_3;
                     case 4:
-                        return MTLLanguageVersion2_4;
+                        if (@available(macOS 12.0, *))
+                        {
+                            return MTLLanguageVersion2_4;
+                        }
+                        assert(0 && "MSL 2.4 requires macOS 12.");
+                        break;
                     default:
                         assert(0 && "Unsupported MSL Minor Language Version.");
                 }
